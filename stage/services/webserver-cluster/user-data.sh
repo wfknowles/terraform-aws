fa5ff83ff758c7215040fa40ee46568910b2e5ea
Will Knowles <will@willknowles.com>
2024-08-15 13:25:08 -0500

    Refactored webserver-cluster to use external shell script for user_data

---
+++ stage/services/webserver-cluster/user-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat > index.html <<EOF
+  <h1>Hello, World</h1>
+  <br/>
+  <p>DB address: ${db_address}</p>
+  <p>DB port: ${db_port}</p>
+EOF
+
+nohup busybox httpd -f -p ${server_port} &
+
+
